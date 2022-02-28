a34c4b38556e2be4a56d7ae9f3733456a250658f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-02-28 07:48:12 -0700

    Initial Commit

---
+++ Flashzilla SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla SwiftUI
+//
+//  Created by Mitch Andrade on 2/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flashzilla SwiftUI/Flashzilla_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashzilla_SwiftUIApp.swift
+//  Flashzilla SwiftUI
+//
+//  Created by Mitch Andrade on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Flashzilla_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
